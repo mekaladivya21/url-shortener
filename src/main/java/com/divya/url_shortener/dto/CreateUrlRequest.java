b94364b71aaa8dce0aad980ee0bceefc147f68b9
mekaladivya21 <39720443+mekaladivya21@users.noreply.github.com>
2026-09-08 18:09:42 -0500

    Add UrlService unit tests

--- src/main/java/com/divya/url_shortener/dto/CreateUrlRequest.java
+++ src/main/java/com/divya/url_shortener/dto/CreateUrlRequest.java
@@ -1,26 +1,41 @@
 package com.divya.url_shortener.dto;
 
+import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-import lombok.Getter;
-import lombok.Setter;
 import org.hibernate.validator.constraints.URL;
 
-@Setter
-@Getter
+import java.time.LocalDateTime;
+
 public class CreateUrlRequest {
 
     @NotBlank(message = "Original URL is required")
-
     @URL(message = "Please provide a valid URL")
-
     @Size(
             max = 2048,
             message = "URL cannot exceed 2048 characters"
     )
     private String originalUrl;
 
+    @Future(message = "Expiration time must be in the future")
+    private LocalDateTime expiresAt;
+
     public CreateUrlRequest() {
     }
 
+    public String getOriginalUrl() {
+        return originalUrl;
+    }
+
+    public void setOriginalUrl(String originalUrl) {
+        this.originalUrl = originalUrl;
+    }
+
+    public LocalDateTime getExpiresAt() {
+        return expiresAt;
+    }
+
+    public void setExpiresAt(LocalDateTime expiresAt) {
+        this.expiresAt = expiresAt;
+    }
 }
--- src/main/java/com/divya/url_shortener/dto/UrlResponse.java
+++ src/main/java/com/divya/url_shortener/dto/UrlResponse.java
@@ -1,19 +1,24 @@
 package com.divya.url_shortener.dto;
 
+import java.time.LocalDateTime;
+
 public class UrlResponse {
 
-    private String originalUrl;
-    private String shortCode;
-    private String shortUrl;
+    private final String originalUrl;
+    private final String shortCode;
+    private final String shortUrl;
+    private final LocalDateTime expiresAt;
 
     public UrlResponse(
             String originalUrl,
             String shortCode,
-            String shortUrl
+            String shortUrl,
+            LocalDateTime expiresAt
     ) {
         this.originalUrl = originalUrl;
         this.shortCode = shortCode;
         this.shortUrl = shortUrl;
+        this.expiresAt = expiresAt;
     }
 
     public String getOriginalUrl() {
@@ -26,5 +31,9 @@ public class UrlResponse {
 
     public String getShortUrl() {
         return shortUrl;
+    }
+
+    public LocalDateTime getExpiresAt() {
+        return expiresAt;
     }
 }

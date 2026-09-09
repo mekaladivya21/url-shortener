9c73f932f40b1e5f9e01c99b61914124b2e538d5
mekaladivya21 <39720443+mekaladivya21@users.noreply.github.com>
2026-09-08 19:57:16 -0500

    Add URL integration tests

---
+++ src/test/java/com/divya/url_shortener/integration/TestConfig.java
@@ -0,0 +1,34 @@
+/*
+package com.divya.url_shortener.integration;
+
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+
+import org.springframework.boot.test.context.TestConfiguration;
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+
+import org.springframework.context.annotation.Bean;
+
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+
+import org.springframework.web.client.RestTemplate;
+
+@TestConfiguration
+public class TestConfig {
+
+    @Bean
+    RestTemplate redirectDisabledRestTemplate() {
+
+        var httpClient =
+                HttpClients.custom()
+                        .disableRedirectHandling()
+                        .build();
+
+        var requestFactory =
+                new HttpComponentsClientHttpRequestFactory(
+                        httpClient
+                );
+
+        return new RestTemplate(requestFactory);
+    }
+}*/
